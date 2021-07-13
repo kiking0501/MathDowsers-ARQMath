8422480488dc195c561673bc4c8a501933cee474
kiking <kiking0501@gmail.com>
2021-07-13 17:14:11 -0400

    Update website

---
+++ docs/main.js
@@ -0,0 +1,20 @@
+function askQuestion() {
+    var question_data;
+    function getQuestion(callback){
+        $.getJSON('../../data/ARQMath/experiments/topics/task1-topics-2021.json', function(data) {
+            callback(data);
+        });
+    }
+    getQuestion(function (data) {
+        question_data = data;
+    })
+    function print() {
+        console.log(question_data);
+    }
+    $(".q-text").show("slow");
+    $(".res").hide();
+}
+
+$(document).ready(function() {
+    $(".q-text").hide();
+})
