3c90210180c631e90c08b42b31d70afca0fc8d3d
Andrew Kane <arkane@cs.uwaterloo.ca>
2023-01-25 19:42:45 -0500

    missing preprocessing script

---
+++ src_2022/main_preprocessing.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#############
+# A main script to run all preprocessing files (python files with prefix "prepro") at once
+# See individual prepro files for the expected outcome.
+
+# Run by "./main_preprocessing.sh"
+
+# Note:
+### For data study, it would be a better start to try the official ARQMath Parser classes first:
+###   https://github.com/ARQMath/ARQMathCode
+
+### The following scripts involve running an earlier version of the official Parsers
+###   with custom modifications to work out the document corpus outputs.
+
+#############
+
+task_counter=0
+declare -a files=(
+    "prepro_00_create_html_manifest.py"
+    "prepro_00_modify_formula_representation.py"
+    "prepro_00_create_thread2formulasTSV.py"
+    "prepro_00_create_initial_parsers.py"
+    "prepro_01_create_raw_maps_from_parsers.py"
+    "prepro_02_modify_raw_map_of_comments.py"
+    "prepro_03_create_more_maps.py"
+    "prepro_04_create_question2title_latex2slt.py"
+)
+task_total="${#files[@]}"
+
+error_exit() {
+    echo "... [Task $1/$2 ERROR] $3 aborted."
+    exit 1
+}
+
+for file in ${files[@]}; do
+    task_counter=$((task_counter+1))
+    echo
+    echo
+    echo "... [Task ${task_counter}/${task_total}] Running $file ..."
+    python3.9 ${file} || error_exit ${task_counter} ${task_total} ${file}
+    echo "... [Task ${task_counter}/${task_total}] Success!"
+done
+
